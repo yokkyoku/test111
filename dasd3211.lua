1d2ec20ebd6c757889566b27da97376a60fa9990
yokky <140402390+yokkyoku@users.noreply.github.com>
2025-05-08 16:37:12 +0300

    Update dasd3211.lua

--- dasd3211.lua
+++ dasd3211.lua
@@ -1826,7 +1826,7 @@ end
                             local Decoration = library:create("Frame", {
                                 Name = "Decoration",
                                 Parent = Button,
-                                BackgroundColor3 = Color3.fromRGB(84, 101, 255),
+                                BackgroundColor3 = Color3.fromRGB(255, 0, 0),
                                 BorderSizePixel = 0,
                                 Size = UDim2.new(0, 1, 1, 0),
                                 Visible = false,
