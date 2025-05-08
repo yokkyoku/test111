5c8f9cad9af08a1f1b9706cfad0928a5608b5817
yokky <140402390+yokkyoku@users.noreply.github.com>
2025-05-08 03:11:55 +0300

    Update dasd3211.lua

--- dasd3211.lua
+++ dasd3211.lua
@@ -365,7 +365,7 @@ function library.new(library_title, cfg_location)
             }, SectionButton)
 
             local UIGradient = library:create("UIGradient", {
-                Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(32, 33, 38)), ColorSequenceKeypoint.new(0.5, Color3.fromRGB(81, 97, 243)), ColorSequenceKeypoint.new(1, Color3.fromRGB(32, 33, 38))},
+                Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 0, 0)), ColorSequenceKeypoint.new(0.5, Color3.fromRGB(255, 0, 0)), ColorSequenceKeypoint.new(1, Color3.fromRGB(255, 0, 0))},
             }, SectionDecoration)
 
             local SectionFrame = library:create("Frame", {
